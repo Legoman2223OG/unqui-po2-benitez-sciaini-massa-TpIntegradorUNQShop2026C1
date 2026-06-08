425d10f4a561283aeaaf334cb5e0d4019b61b633
Sciaini, Tomas <tsi_001@hotmail.com>
2026-06-08 19:59:34 -0300

    Refactor de la Clase para poder testear la logica. antes estaban la mayoria de los metodos concretos de la clase vacios y no se podia testear la logica de la clase, ahora se implementaron los metodos concretos y se pueden testear.

--- src/main/java/unqshop/pagos/TarjetaCredito.java
+++ src/main/java/unqshop/pagos/TarjetaCredito.java
@@ -3,8 +3,16 @@ package unqshop.pagos;
 
 public class TarjetaCredito extends MetodoPago {
 
-    public TarjetaCredito(double monto) {
+    private final String numeroTarjeta;
+    private final String cvv;
+    private final int fechaVencimiento; // SOLO AÑO!
+    private boolean datosValidados; //FLAG
+
+    public TarjetaCredito(double monto, String numeroTarjeta, String cvv, int fechaVencimiento) {
         super(monto);
+        this.numeroTarjeta = numeroTarjeta;
+        this.cvv = cvv;
+        this.fechaVencimiento = fechaVencimiento;
     }
 
     //Metodos concretos
@@ -15,12 +23,14 @@ public class TarjetaCredito extends MetodoPago {
         validarNumeroDeTarjeta();
         validarCVV();
         validarVencimiento();
-
+        this.setDatosValidados(true); // si todo pasa: FLAG ==TRUE
     }
 
 
     @Override
     protected void reservarFondos() {
+        //mismo caso q billeteraVirtual.
+        if (!datosValidados) throw new IllegalStateException();
         preAutorizarBancoEmisor();
     }
 
@@ -45,25 +55,46 @@ public class TarjetaCredito extends MetodoPago {
     // ------------------------------------------
 
     //METODOS PARTICULARES DE LA CLASE:
+
     // validar_datos:
+    //--------------------
     private void validarVencimiento() {
+        //CONSEGUIR EL TIMPO ACTUAL
+        int aÑoActual = java.time.Year.now().getValue();
+
+        if (this.fechaVencimiento < aÑoActual) {
+            throw new IllegalArgumentException("Tarjeta vencida.");
+        }
+
     }
 
+
     private void validarCVV() {
+        if (cvv == null || cvv.length() != 3) {
+            throw new IllegalArgumentException("CVV inválido.");
+        }
     }
 
     private void validarNumeroDeTarjeta() {
+        if (numeroTarjeta == null || numeroTarjeta.length() != 10) {
+            throw new IllegalArgumentException("Número de tarjeta inválido.");
+        }
     }
 
+    //-------------------------
     // reservarFondos:
     private void preAutorizarBancoEmisor() {
-
+        System.out.println("Pre-Autorizando $" + this.getMonto() + "En el banco Emisor.");
     }
 
-    //ejecutarTransaccion
+    //ejecutarTransaccion:
     private void debitoDiferido() {
+        System.out.println("Débito diferido con monto $" + getMonto());
     }
 
+    public void setDatosValidados(boolean datosValidados) {
+        this.datosValidados = datosValidados;
+    }
 
     // ------------------------------------------
     // ------------------------------------------
