cc4cf46b3cd9bb1837bdddfd01d91189e847399f
Lautaro Benitez Jara <lautaro.benitezjara76@gmail.com>
2026-06-10 19:16:32 -0300

    metodos confirmar preparar enviar entregrar cancelar

--- src/main/java/unqshop/Pedido/Pedido.java
+++ src/main/java/unqshop/Pedido/Pedido.java
@@ -1,5 +1,36 @@
 package main.java.unqshop.Pedido;
 
 public class Pedido {
-
+	private List <CatalogoItem> items; /*items que pide el cliente en el pedido*/
+	private List <ObserverPedido> subSistemas; /*se les notifica cuando hay cambio de estado en el pedido*/
+	private EstadoPedido estado; /*etapa del ciclo de vida del pedido*/
+	private Cliente cliente; /*cliente que realizo el pedido*/
+	
+	/*Transiciones validas:  
+	 * BORRADOR -> CONFIRMADO
+	 * BORRADOR -> CANCELADO
+	 * 
+	 * CONFIRMADO -> EN_PREPARACION
+	 * */
+	
+	public void confirmar() {/*Valido solo en BORRADOR*/
+		
+	}
+	
+	public void preparar() {/*Valido solo en CONFIRMADO*/
+		
+	}
+	
+	public void enviar() {/*Valido solo eb EN_PREPARACION*/
+		
+	}
+	
+	public void entregar() {/*Solo valido en ENVIADO*/
+		
+	}
+	
+	public void cancelar() {/*valido en BORRADOR, CONFIRMADO, EN_PREPARACION, ENVIADO*/
+		
+	}
+	
 }
