2a449afb1732a273b2d12edccbed3fb7f4f41896
Sciaini, Tomas <tsi_001@hotmail.com>
2026-07-02 19:05:47 -0300

    agrego linea que quedo colgada para aclara contexto de la cita del libro de gof

--- src/main/java/unqshop/reportes/ReporteDeProductosMasVendidos.java
+++ src/main/java/unqshop/reportes/ReporteDeProductosMasVendidos.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 /*
+*libro gof:
 * ConcreteElement (AssignmentNode,VariableRefNode)
 - implements an Accept operation that takes a visitor as an argument
 * */
