0639b8cf7b73196569d917c8b3b6246c95f8ad77
Sciaini, Tomas <tsi_001@hotmail.com>
2026-07-02 16:21:43 -0300

    add comentario

--- src/main/java/unqshop/envios/Enviable.java
+++ src/main/java/unqshop/envios/Enviable.java
@@ -1,7 +1,7 @@
 package main.java.unqshop.envios;
 
 public interface Enviable {
-
+ ///cada MetodoEnvio recibe un Enviable y saca solo lo que necesita.
     double total();
 
     double pesoTotal();
