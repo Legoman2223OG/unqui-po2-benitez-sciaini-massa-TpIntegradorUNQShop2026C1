47ee8ce584781fed7eac6d74a5d1e1b39a07cdb7
Sciaini, Tomas <tsi_001@hotmail.com>
2026-07-03 17:54:30 -0300

    comentario importante post correction del profesor

--- src/main/java/unqshop/envios/EnvioFacade.java
+++ src/main/java/unqshop/envios/EnvioFacade.java
@@ -1,5 +1,8 @@
 package main.java.unqshop.envios;
-
+/*
+* Esta fachada queda obsoleta luego de la correction del 03/07/2026 @17.00hs
+* no se debe aplicar en la APP.
+* */
 public class EnvioFacade { 
  
     public MetodoEnvio envioEstandar() {
